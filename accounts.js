725cefe3906db0453de21e471dbcd74060534226
joserw1 <iboysganteng@gmail.com>
2026-03-15 09:15:16 +0700

    Create accounts.js

---
+++ accounts.js
@@ -0,0 +1,16 @@
+{
+  "accounts": [
+    {
+      "name": "Akun Tumbal 1",
+      "token": "MASUKIN_TOKEN_DISINI",
+      "guildId": "MASUKIN_GUILD_ID_DISINI",
+      "voiceChannelId": "MASUKIN_VOICE_ID_DISINI"
+    },
+    {
+      "name": "Akun Tumbal 2",
+      "token": "MASUKIN_TOKEN_AKUN_2",
+      "guildId": "MASUKIN_GUILD_ID_2",
+      "voiceChannelId": "MASUKIN_VOICE_ID_2"
+    }
+  ]
+}
